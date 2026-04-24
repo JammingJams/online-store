20acdd6cb2872cf2e1a1fc2a24802a5024a064a9
JammingJams <noahlogan20o00@gmail.com>
2026-04-24 11:26:19 -0400

    Added a search function to the store!

--- src/main/java/com/pluralsight/OnlineShopApp.java
+++ src/main/java/com/pluralsight/OnlineShopApp.java
@@ -1,4 +1,96 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class OnlineShopApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //User shopingcart hashmap
+        //Product display hashmap
+        String userInput = "";
+        ArrayList<Product> products = Product.getProductList();
+        //ArrayList<Product> search = Product.searchProducts(products, userInput);
+
+        boolean userInApp = true;
+        boolean userInProduct = true;
+        boolean userInCheckout = true;
+
+        while (userInApp) {
+
+            userInProduct = true;
+            userInCheckout = true;
+            System.out.println("Welcome to the Online.complex!");
+            System.out.println("(P) Displays our products!");
+            System.out.println("(D) Displays whats in your cart!");
+            System.out.println("(X) Leave the complex :(");
+            userInput = sc.nextLine().replaceAll("\\s+","").trim().toUpperCase();
+            switch (userInput) {
+                case ("P") -> {
+                    for (Product c : products) {
+                        System.out.printf("%s|%s|$%.2f|%s\n", c.getSKU(), c.getProductName(), c.getPrice(), c.getDepartment());
+                    }
+                    while (userInProduct) {
+                        //This will be adding to your cart
+                        System.out.println("(S) Search and filter out items you want to buy!");
+                        System.out.println("(A) Add a product into your cart!");
+                        System.out.println("(X) Exit out into the homepage!");
+                        userInput = sc.nextLine().replaceAll("\\s+","").trim().toUpperCase();
+                        switch (userInput) {
+                            case ("S") -> {
+                                System.out.println("Search by Product Name, Price, or Department!");
+                                userInput = sc.nextLine().replaceAll("\\s+","").trim().toLowerCase();
+                                ArrayList<Product> search = Product.searchProducts(products, userInput);
+
+                                if (search.isEmpty()) {System.out.println("Product not currently available!");}
+                                else {
+                                    for (Product p : search) {
+                                        System.out.printf("%s|%s|$%.2f|%s\n", p.getSKU(), p.getProductName(), p.getPrice(), p.getDepartment());
+                                    }
+                                }
+
+                            }
+                            case ("A") -> {
+                                System.out.println("Hey this works!");
+                            }
+                            case ("X") -> {userInProduct = false;}
+                            default -> {System.out.println("Incorrect userInput!");}
+                        }
+
+                    }
+
+                }
+
+                case ("D") -> {
+                    while (userInCheckout) {
+                        //This will be the Checkout!
+                        System.out.println("(C) Checkout all your products!");
+                        System.out.println("(R) Removes products from your cart!");
+                        System.out.println("(X) Exit out into the homepage!");
+                        userInput = sc.nextLine().replaceAll("\\s+","").trim().toUpperCase();
+                        switch (userInput) {
+                            case ("C") -> {
+                                System.out.println("Hey this works!!");
+                            }
+                            case ("R") -> {
+                                System.out.println("Hey this works!");
+                            }
+                            case ("X") -> {userInCheckout = false;}
+                            default -> {System.out.println("Incorrect userInput!");}
+                        }
+
+                    }
+                }
+
+                case ("X") -> {userInApp = false;}
+
+                default -> {System.out.println("Incorrect userInput!");}
+
+
+            }
+
+        }
+
+    }
+
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,81 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class Product {
+    private String SKU;
+    private String productName;
+    private double price;
+    private String department;
+
+    public Product() {
+        this.SKU = SKU;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    //Getters are here!
+    public String getSKU() {return SKU;}
+    public String getProductName() {return productName;}
+    public double getPrice() {return price;}
+    public String getDepartment() {return department;}
+
+    //Setters are here!
+    public void setSKU(String SKU) {this.SKU = SKU;}
+    public void setProductName(String productName) {this.productName = productName;}
+    public void setPrice(double price) {this.price = price;}
+    public void setDepartment(String department) {this.department = department;}
+
+    //This reads the products file and organizes it
+    public static ArrayList<Product> getProductList() {
+        ArrayList<Product> listProducts = new ArrayList<>();
+    try {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/products.csv"));
+
+        String text;
+        bufferedReader.readLine();
+
+        while ((text = bufferedReader.readLine()) != null) {
+
+            String[] tempArray = text.split("\\|");
+
+            Product items = new Product();
+
+            try {
+                items.setSKU(tempArray[0]);
+                items.setProductName(tempArray[1]);
+                items.setPrice(Double.parseDouble(tempArray[2]));
+                items.setDepartment(tempArray[3]);
+            }
+            catch (NumberFormatException e) {e.printStackTrace();}
+
+            listProducts.add(items);
+        }
+        bufferedReader.close();
+
+    }
+    catch (IOException e) {e.printStackTrace();}
+    return listProducts;
+    }
+
+    //This method helps with searching for an item
+    public static ArrayList<Product> searchProducts(ArrayList<Product> products, String userInput) {
+        ArrayList<Product> results = new ArrayList<>();
+        for (Product p : products) {
+            if
+                (p.getProductName().toLowerCase().contains(userInput)
+                || p.getDepartment().toLowerCase().contains(userInput)
+                || p.getSKU().toLowerCase().contains(userInput)) {
+                results.add(p);
+            }
+        }    return results;
+    }
+
 }
+
